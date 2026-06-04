1210a9b19f11e4734f88a441904516793c33d264
Safiya-Parveen <safiyaparveen64@gmail.com>
2026-06-05 00:25:40 +0530

    Updating HTML,CSS and JavaScript

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const APIkey = "b6eba738f3556a892d97618350d974ee";
-const APIurl = "https://api.openweathermap.org/data/2.5/weather?q=germany&appid=b6eba738f3556a892d97618350d974ee&units=metric";
+const APIurl = "https://api.openweathermap.org/data/2.5/weather?&appid=b6eba738f3556a892d97618350d974ee&units=metric&q=";
 
-const searchbox = document.querySelector("search input");
-const searchbutton = document.querySelector("search button");
+const searchbox = document.querySelector(".search input");
+const searchbtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkweather(city) {
-    const response = await fetch(APIurl + city + `&appid=${APIkey}`);
+    const response = await fetch(APIurl + city + "&appid" + APIkey);
     var data = await response.json();
+
+    if (response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }
+
+    else {
+        if (data.weather[0].main == "Clouds") {
+            weatherIcon.src = "images/clouds.png";
+        }
+        else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "images/rain.png";
+        }
+        else if (data.weather[0].main == "Mist") {
+            weatherIcon.src = "images/mist.png";
+        }
+        else if (data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "images/drizzle.png";
+        }
+        else if (data.weather[0].main == "Clear") {
+            weatherIcon.src = "images/clear.png";
+        }
+        else if (data.weather[0].main == "Snow") {
+            weatherIcon.src = "images/snow.png";
+        }
+    }
 
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
@@ -14,11 +41,11 @@ async function checkweather(city) {
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 
 
-
-    console.log(data);
-
-
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
 }
-searchbutton.addEventListener("click", () => {
+
+
+searchbtn.addEventListener("click", () => {
     checkweather(searchbox.value);
 });
